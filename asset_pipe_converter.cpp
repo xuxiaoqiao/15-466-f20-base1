319ab2642db5139b9653bad1c127fedef27701f1
Xiaoqiao Xu <xiaoqiao@andrew.cmu.edu>
2020-09-11 10:47:42 -0400

    asset pipe: partial impl

--- asset_pipe_converter.cpp
+++ asset_pipe_converter.cpp
@@ -4,12 +4,14 @@
 #include <string>
 #include <stdexcept>
 #include <fstream>
+#include <utility>
+#include <algorithm>
+#include <cassert>
 
 #include <glm/glm.hpp>
 
 #include "PPU466.hpp"
 #include "read_write_chunk.hpp"
-
 
 constexpr char USAGE_PROMPT[] = R"(
 usage:
@@ -50,7 +52,7 @@ struct ProcessedSprites {
 	std::vector<PPU466::Tile> tiles;
 	std::vector<PPU466::Palette> palettes;
 	// mapping: ( key: resource name, value: (tile_index, palette_index))
-	std::map<std::string, std::pair<int,int>> mapping;
+	std::map<std::string, std::pair<int, int>> mapping;
 };
 /**
  * read raw sprite images and attempt to convert to ProcessedSprites, which contains
@@ -101,8 +103,82 @@ std::map<std::string, ImgContent> load_raw_sprite_images(const std::string &tile
 }
 
 ProcessedSprites process_sprite_images(const std::map<std::string, ImgContent> &raw_images) {
+	// TODO(xiaoqiao): origin point of images? start from lower-left corer?
 	// TODO: implement me
-	return ProcessedSprites();
+	std::vector<PPU466::Tile> tiles;
+	std::vector<PPU466::Palette> palettes;
+	std::map<std::string, std::pair<int,int>> mapping;
+	for (const auto &img_iterator : raw_images) {
+		const std::string &name = img_iterator.first;
+		const ImgContent &img = img_iterator.second;
+		int tile_index = -1;
+		int palette_index = -1;
+		if (img.height != 8 || img.width != 8) {
+			throw AssetConversionException(
+				std::string("Invalid PNG asset size for ") + name + ". Should be 8x8, but actually"
+					+ std::to_string(img.width) + "x" + std::to_string(img.height));
+		}
+		std::vector<glm::u8vec4> colors;
+		// first pass: what's the colors in this img
+		for (const auto &pix : img.data) {
+			auto it = std::find(colors.begin(), colors.end(), pix);
+			if (it != colors.end()) {
+				// no action required
+			} else {
+				colors.push_back(*it);
+				if (colors.size() > 4) {
+					std::string errmsg = std::string("Too many colors used in asset " + name + ".");
+					throw AssetConversionException(errmsg);
+				}
+			}
+		}
+		// sort colors in a canonical order: First order by alpha in desc order,
+		// on tie, order by red in desc order, then by green, then by blue
+		std::sort(colors.begin(), colors.end(),
+				  [](const glm::u8vec4 &a, const glm::u8vec4 &b) -> bool {
+					  if (a[3] != b[3]) {
+						  return a[3] < b[3];
+					  } else if (a[2] != b[2]) {
+						  return a[2] < b[2];
+					  } else if (a[1] != b[1]) {
+						  return a[1] < b[1];
+					  } else {
+						  return a[0] < b[0];
+					  }
+				  });
+		assert(colors.size() <= 4);
+		// pad zeroes if colors.size() < 4;
+		colors.resize(4, glm::u8vec4{0, 0, 0, 0});
+		PPU466::Palette p;
+		std::copy(colors.begin(), colors.end(), p.begin());
+		const auto palette_it = std::find(palettes.begin(), palettes.end(), p);
+		if (palette_it != palettes.end()) {
+			palette_index = std::distance(palettes.begin(), palette_it);
+		} else {
+			palettes.push_back(p);
+			palette_index = palettes.size() - 1;
+		}
+		// TODO(add check for palettes size too long)
+		// second pass: convert the png content to tiles
+		// TODO(xiaoqiao): is this zero initialization?
+		PPU466::Tile t{};
+		for (int i = 0; i < 8 * 8; i++) {
+			int row_idx = i / 8;
+			int col_idx = i % 8;
+			const auto &pix = img.data[i];
+			assert(std::find(p.begin(), p.end(), pix) != p.end());
+			int pixel_color_idx_in_palette = std::distance(p.begin(), std::find(p.begin(), p.end(), pix));
+			assert(pixel_color_idx_in_palette < 4);
+			uint8_t bit0 = pixel_color_idx_in_palette & 1;
+			uint8_t bit1 = pixel_color_idx_in_palette >> 1;
+			t.bit0[row_idx] |= (bit0 << col_idx);
+			t.bit1[row_idx] |= (bit1 << col_idx);
+ 		}
+		tiles.push_back(t);
+		tile_index = tiles.size() - 1;
+		mapping[name] = std::make_pair(tile_index, palette_index);
+	}
+	return ProcessedSprites{std::move(tiles), std::move(palettes), std::move(mapping)};
 }
 
 void store_sprite_resources(
